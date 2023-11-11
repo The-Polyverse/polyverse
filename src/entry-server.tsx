343a1e540acd41281650ec0085fc6d817536c055
Ivgeni <ivgenis@gmail.com>
2023-11-11 02:06:15 +0000

    dom hydration and cleanup of files

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -1,11 +1,13 @@
-import { renderToString } from "react-dom/server";
-import App from "./App";
 import React from "react";
+import { renderToString } from "react-dom/server";
+
+import App from "./App";
+import "./index.css";
 
 export async function render(url: string, template: string) {
   console.log("rendering", url);
   return template.replace(
-    '<div id="root"></div>', 
+    '<div id="root"></div>',
     renderToString(
       <div id="root">
         <React.StrictMode>

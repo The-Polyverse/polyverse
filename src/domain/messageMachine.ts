d7e255ca3f6e89e79549d99686febd172ff570b4
Ivgeni <ivgenis@gmail.com>
2023-12-09 18:00:19 +0000

    we got ourselves a message entity

--- src/domain/messageMachine.ts
+++ src/domain/messageMachine.ts
@@ -10,7 +10,7 @@ import { createMachine } from "xstate";
  * @xstate-config N4IgpgJg5mDOIC5QFs6wIYwHQEsB2OALjugDY4BekAxAMYBOY6hYA2gAwC6ioADgPawiOfnh4gAHogCMADgCsWefIAsANlnsVAZgBM22erUAaEAE9E86eywqAnA4Ds2+Y7UrrugL5fTqWBjY+MJklDS0pKJsXOICQsSi4lIIcorK6po6+oZqJuaIKvJ2WA4OstJ2jp52rj5+aJhguATEoVQQ1IyE9DhgAG7R3EggccKJw8mpSqoaWnoGRqYWCNqOuliyjvZqRVba0o7OdSD+gU3BreTtnXCE-IwcQ3yCY2ITMgrTGXPZi-kI5SU7GB0lUKkM8jUx1OjSwDCYLA6fVCEGYg1iLwSb1AyR02hKukq7DcukcWnUS0s7Hxunsdg0hi29m00Ia2C6PX6NAArrxUSxHhj4iJsZICtp8XZCWSSWSVBT-gZ1nSakZ2JC1FtWQFYRzegMOhAwKQwAKYsNRlikuLJdLiWpSeS8stdGppCVSg6VM5gXYVNqzlg9VyOuh6LQABY4AaCi2YkXWhB4glE2VOykIXTsda00q6WS5VXEgOw5Hkfk0XhgehCWCEWPPYXjHEyOQ2KqbB3E6TgtwZpW2UozNTq3Ja3wnNlNXkVpEotENkbx5tigH6D26HsKFTAgvSfuyZVDtUa8f1HXYMOR6M0Rh1+7ouNN0XJOW2EdSke
  */
 const messageMachine = createMachine({
-  /** @xstate-layout N4IgpgJg5mDOIC5QFs6wIYwHQEsB2OALjugDY4BekAxAMYBOY6hYA2gAwC6ioADgPawiOfnh4gAHogCMADgCsWefIAsANlkqAnACZ5AdnnsVAGhABPRPOnss2rVuk7n0gMwrNAX09nUsDNj4wmSUNLSkomxc4gJCxKLiUghyisrqmroGRqYWiCryWlgODq7ssuyuamr53r5omGC4BMQhVBDUjIT0OGAAblHcSCCxwglDSSlKqhraeobGZpYIrvo6WLL62gVqro7yu7UgfgGNQS3kbR1whPyMHIN8gqNi4zIKU+mzWQu5CLLSSnYQJs8h0Wk0ai0+kOxwaWAYTBY7V6IQgzAGMSe8ReoCSKlcriKYLU1iqWgJPyWRkJOns+X07Fp0khMPq2E63T6NAArrw0Sx7pi4iIcZI8gSiVoSczIRScktXLI1kYgeppPk1M4dGpWf44Ryev12hAwKQwALokMRtjEuLCbopaTZaV5YhtQDilojBo9CppLqTlgDVz2uh6LQABY4fqCq1YkW2hD4+3Ep3kl2LN2MrB6IHsJyKjSyXQBuEo8j8mi8MD0ISwQixx7Csa4mRyWz6OSa5wkilaTPLJWA1VqdXybva0vYXmV5Go9GN4bxltiv46e26MdaDwqHTsNQDxXKvNqjVanU+I5sxphyPRmiMeu3DFx5uipIMlR2NTsLS-2TuNI4IDnuay5sC66yEWJaXrC7LXM+c4VgulpNs8ibkoUu7SPo+hVH625OAOQGKHmqoEgYfoXpeeD8Ca8BDHBYBCuhrwIAAtAevzsYonp8Z6+haFOpzNCQFyQCxNpsbuA72HY9g7Cse5ksJ8KMOiECSQmbGlGoWDsKsf6uMy0jbNIA7UlgmwOBBOzkv6sHXkG5qckaWkrkkLpKKZmpKsoBI7Poh76ISKwqqCpk4YY0KOXq2Dljgs7ue+MjsAU6z4iShHqsZ-a-EeRTFKoJSUUJsWBtWtY4PWEmvqxrbJGCX75qozjbioqzyIe-xWXSbiOEeXqqTOGnJYmn7fr+-6AcB+VDp6xXkqVqkmmaSJjWxaRYK41j7Co+KyEqv4gaOOb2EqJKBTosiqbeUZuXVUkNeOelpeo1TMr6h0gdm8wQYWh0wXUcWNI+NyMJpj3aQ1SobjoW47spxFesO5E7ZszLeN4QA */
+  /** @xstate-layout N4IgpgJg5mDOIC5QFs6wIYwHQEsB2OALjugDY4BekAxAMYBOY6hYA2gAwC6ioADgPawiOfnh4gAHogBMAFmlYArADZ2AdjkBmaQE45ADnUAaEAE9EARn0LFizWsXTV+ncs3qAvh5OpYGbPjCZJQ0jIT0OGAAbmxc4gJCxKLiUggWyopKytKK7KoqOpqaKibmCPKyWHnKNe4Waq5ePmiYYLgExMFUENQc3EggCcLJA6nS+pUORWp5heyyFrLKpYj6Fkrsm+zS0uo79spNIL7+bQxMLD1RwRDMsf18gsNio4hqFuuyeU7bakuyigsKwQmmU+iw6RqGUW7E0AKOJ1aWDCEWiNFopFE93iTySL1AqVkcKwTkMOmsFh07EUC2Bmn0yiwekBcPmdh0CwRLWwKMiMR6AFdeLcWH0cYkRPjJIhZF8qkt9C4aTpydIgWZEKoFAtaq5QVZ0ly-EjeWiehAwKQwKK4gMhniUjL7FV9IodLkrAyZuqyktNEz3NINNlFPoGoojadkdbUfzqOh6LQABY4GJiu24yWO8py+ZgpWyFUU4EGEm5TbyOxuTQ6SNI67kEU0XhgehCWCEdOPCUjAmWSn+lQ5FTpelWEtyyHKDQTZTuuQR7zHbltBs4Js9FttnAd1gWB6DTO96VpAdZYfKUdrfTA2RrLBQmoWcYzBZwuvYIUb6hrptdw89lKqT1OwOgkvI0igjofwAm6dKXlgOrQpSqh6GoH5tAmyapqEcCEPwjD-vaWavAg7yfN8Wp-MoAI+jIshqIhkIzHosKbHYGFYFhKZxlu7adra3bPNm5GIZRvz-ICwIfGBSEzHOiy2HonGMB2BE0L+dxEUeQGIByjFOLYbjZNBhiaBOjFyaBuzuNSmica29DqYKwpaYJAHCaRFiKA0JIZDUagMhMazLBq5T3o+l4vvMFj2UceD8Ba8ADIiMDip5fYIAAtDUfmUq4NJ3tBJRhVl4I5FsmyuqB1iFJxgSdOQ3TpQ6pFZVYeUqhksouA4JVlL5FWbA02jGRYnHnHcEAtSRmU+YySGXvY6gTHRIJgg+j6Al8cKLs0xo8jGfKQDNx5jO6VRqKChjzKxmhrX6AbbIqhbTvooJ7cuB2rjcU2nbpaR5IxYJ-DM7hRfI8GfFO8lzq4YacXxO6XP92aLLsWBg95ao4zkaglvdmOFiq8lFdZnFfn9GaAWjLEPmGgIWHkz7sC4wJuv6ujpKGNbycUnEWlaKPUxlJ4qP6DQgyy05qBkt7ZPTz3bNjd7oUuqWYYmPEnSLrVzYFVSVTJOTyKFZRyOs5abJehjjD5av7VGqn4Yw0267NJ7ves1hrLCSxXRo5lhXIjE6Mx1lsXZDn0E5ruo15soS1YfzPsUmzure1gQo+z5hjF75eB4QA */
   types: {} as {
     context: {
       id: string;
@@ -51,9 +51,13 @@ const messageMachine = createMachine({
     initialized: {
       on: {
         create: "created",
-        clone: "created",
         retrieve: "retrieved",
-        restore: "restored",
+      },
+      always: {
+        target: "archived",
+        guard: "isArchived",
+        // @ts-expect-error the visual editor doesn't support the `cond` property yet
+        cond: "isArchived",
       },
     },
     created: {
@@ -63,6 +67,7 @@ const messageMachine = createMachine({
     },
     retrieved: {
       on: {
+        clone: "created",
         update: "updated",
         delete: "deleted",
         archive: "archived",
@@ -70,7 +75,17 @@ const messageMachine = createMachine({
     },
     validated: {
       on: {
-        persist: "persisted",
+        persist: [
+          {
+            target: "persisted",
+            guard: "isValid",
+            // @ts-expect-error the visual editor doesn't support the `guard` property yet
+            cond: "isValid",
+          },
+          {
+            target: "errored",
+          },
+        ],
       },
     },
     persisted: {
@@ -87,11 +102,17 @@ const messageMachine = createMachine({
     archived: {
       on: {
         restore: "restored",
+        persist: "persisted",
       },
     },
     restored: {
       on: {
         validate: "validated",
+      },
+    },
+    errored: {
+      on: {
+        update: "updated",
       },
     },
   },

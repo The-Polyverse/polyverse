d49fba23491b86bee06fedaba60bb94027b0ea01
Ivgeni <ivgenis@gmail.com>
2023-12-19 17:54:45 +0000

    created and retrieved transitions to always happen from initialized

--- src/domain/messageMachine.ts
+++ src/domain/messageMachine.ts
@@ -10,7 +10,7 @@ import { createMachine } from "xstate";
  * @xstate-config N4IgpgJg5mDOIC5QFs6wIYwHQEsB2OALjugDY4BekAxAMYBOY6hYA2gAwC6ioADgPawiOfnh4gAHogCMADgCsWefIAsANlnsVAZgBM22erUAaEAE9E86eywqAnA4Ds2+Y7UrrugL5fTqWBjY+MJklDS0pKJsXOICQsSi4lIIcorK6po6+oZqJuaIKvJ2WA4OstJ2jp52rj5+aJhguATEoVQQ1IyE9DhgAG7R3EggccKJw8mpSqoaWnoGRqYWCNqOuliyjvZqRVba0o7OdSD+gU3BreTtnXCE-IwcQ3yCY2ITMgrTGXPZi-kI5SU7GB0lUKkM8jUx1OjSwDCYLA6fVCEGYg1iLwSb1AyR02hKukq7DcukcWnUS0s7Hxunsdg0hi29m00Ia2C6PX6NAArrxUSxHhj4iJsZICtp8XZCWSSWSVBT-gZ1nSakZ2JC1FtWQFYRzegMOhAwKQwAKYsNRlikuLJdLiWpSeS8stdGppCVSg6VM5gXYVNqzlg9VyOuh6LQABY4AaCi2YkXWhB4glE2VOykIXTsda00q6WS5VXEgOw5Hkfk0XhgehCWCEWPPYXjHEyOQ2KqbB3E6TgtwZpW2UozNTq3Ja3wnNlNXkVpEotENkbx5tigH6D26HsKFTAgvSfuyZVDtUa8f1HXYMOR6M0Rh1+7ouNN0XJOW2EdSke
  */
 const messageMachine = createMachine({
-  /** @xstate-layout N4IgpgJg5mDOIC5QFs6wIYwHQEsB2OALjugDY4BekAxAMYBOY6hYA2gAwC6ioADgPawiOfnh4gAHogBMAFmlYArADZ2AdjkBmaQE45ADnUAaEAE9EARn0LFizWsXTV+ncs3qAvh5OpYGbPjCZJQ0jIT0OGAAbmxc4gJCxKLiUggWyopKytKK7KoqOpqaKibmCPKyWHnKNe4Waq5ePmiYYLgExMFUENQc3EggCcLJA6nS+pUORWp5heyyFrLKpYj6Fkrsm+zS0uo79spNIL7+bQxMLD1RwRDMsf18gsNio4hqFuuyeU7bakuyigsKwQmmU+iw6RqGUW7E0AKOJ1aWDCEWiNFopFE93iTySL1AqVkcKwTkMOmsFh07EUC2Bmn0yiwekBcPmdh0CwRLWwKMiMR6AFdeLcWH0cYkRPjJIhZF8qkt9C4aTpydIgWZEKoFAtaq5QVZ0ly-EjeWiehAwKQwKK4gMhniUjL7FV9IodLkrAyZuqyktNEz3NINNlFPoGoojadkdbUfzqOh6LQABY4GJiu24yWO8py+ZgpWyFUU4EGEm5TbyOxuTQ6SNI67kEU0XhgehCWCEdOPCUjAmWSn+lQ5FTpelWEtyyHKDQTZTuuQR7zHbltBs4Js9FttnAd1gWB6DTO96VpAdZYfKUdrfTA2RrLBQmoWcYzBZwuvYIUb6hrptdw89lKqT1OwOgkvI0igjofwAm6dKXlgOrQpSqh6GoH5tAmyapqEcCEPwjD-vaWavAg7yfN8Wp-MoAI+jIshqIhkIzHosKbHYGFYFhKZxlu7adra3bPNm5GIZRvz-ICwIfGBSEzHOiy2HonGMB2BE0L+dxEUeQGIByjFOLYbjZNBhiaBOjFyaBuzuNSmica29DqYKwpaYJAHCaRFiKA0JIZDUagMhMazLBq5T3o+l4vvMFj2UceD8Ba8ADIiMDip5fYIAAtDUfmUq4NJ3tBJRhVl4I5FsmyuqB1iFJxgSdOQ3TpQ6pFZVYeUqhksouA4JVlL5FWbA02jGRYnHnHcEAtSRmU+YySGXvY6gTHRIJgg+j6Al8cKLs0xo8jGfKQDNx5jO6VRqKChjzKxmhrX6AbbIqhbTvooJ7cuB2rjcU2nbpaR5IxYJ-DM7hRfI8GfFO8lzq4YacXxO6XP92aLLsWBg95ao4zkaglvdmOFiq8lFdZnFfn9GaAWjLEPmGgIWHkz7sC4wJuv6ujpKGNbycUnEWlaKPUxlJ4qP6DQgyy05qBkt7ZPTz3bNjd7oUuqWYYmPEnSLrVzYFVSVTJOTyKFZRyOs5abJehjjD5av7VGqn4Yw0267NJ7ves1hrLCSxXRo5lhXIjE6Mx1lsXZDn0E5ruo15soS1YfzPsUmzure1gQo+z5hjF75eB4QA */
+  /** @xstate-layout N4IgpgJg5mDOIC5QFs6wIYwHQEsB2OALjugDY4BekAxANoAMAuoqAA4D2sRO7eLIAD0QBGAGwBWLONEAmcfRmiAzOLn1hAGhABPRAA5hU+sZkzhAFmmjhAdhkBfe1tSwM2fNzKUatYcyQgHFzEvPxCCGKS0mqKKmqaOiIyNkbG9OLCwgbmWTaOzmiYYLgExF5UEHQy-myc3KEB4ZFSsvKxqvIJugjmMuZY9KJDSnpKYxIAnHlOIC5uxQDGAE5g6IQ0AG5eEGtgDDWBdSF8jYg2Q1I22emi5vRK5jZa3UoSWBNiovQT9DY2E3clPlZoVsCtCEscGANjQFqReHsmPwgvUTqBwuYJpIzHp-mZHsIFKJnoglDYlFhPso7ENxOcZECZnMilhwZDoTQAK6sHbrfbIo48NGCRCY7G5Cb4myExQkhBfeiU2l6ei9UTnUQTYHMsFgCFQmGVCBgUh6xEHFHHMKirFYHF4izSoly3qSIZDcm2cwjW6ibWg4psg00dBLBYACxwMP5AUtQutPVt9sljplxMSCD6kgmOZzQ1VI2EEz0-tcLK25F5NFYYCWXFghBjtWC8dOET6eiw5O9egmZMlyXMcoZKQ++d6j1+dIcTIDWArOCrlRrdZwDd8FsFDXRSXMne7Iz7eMHLv+WByw3M1mLNg6pfmWG5S+oC6rTcOLe3Ioiyn63ps5iYv8-wSEOGYqJ27riBMyiDFMMglrOZbYKGEZRjQKwNuwKzvnGX7hBqlzXNIgKPMOlhKu6SjJISe5DPeLKoZGhrUCu9aNkisZbsKBEXHSxG3PcZEZpkKRUsI9q2Ho9FIQ+mGENhmzbLsuHcQmhIKOeZLiNB0hXL+5GSFSyg0aq0l+rJLK1ksimVE+Kmcc2qLqb+WkAUBUwweIYHdI8Eznp8DzXrid7Ang7DGvAAQ6mAAqfjxiAALTpt0yUDGkGWZSo4gMe4pQkOQFRxc5ba9HKEmSABObZLYxamMIuWLCsuwQMVVptsohjiHoeh0g8whjN8Exyq8ogBeq6jdUoWKEtMBTIYGersoabWtjuCCvJVMG9tRgGqsI4guqYWBKJ8eh3ANxgGI187KesrVcfF6kSTIWC9QCqinVkQx6CN0GUdBsGanYiHzQ+bFrvdq34SIjySCFV0-A85zDjI-mYjmmKmHcIUzmDLL2VDj0letti3G90HevIfn-L9GbQYY+3SBJNj3Adc0ggtWDGqaRNOe162yIYV49QoJEKAhR39GO0GnVM3bmDdTHoQ9-Nrd+ygUrYWSeTj51PBmDL+X0uaShk9B6GjOWWbqWErKrH4k9+PVjabuLpPIYyGVg7rDKZdEWfj2DWbZ0MJREmKdr2gxZBb3mDIdGZ+e8gVXkWuPW44QA */
   types: {
     context: {} as {
       id: string;
@@ -37,16 +37,26 @@ const messageMachine = createMachine({
   initial: "initialized",
   states: {
     initialized: {
-      on: {
-        create: "created",
-        retrieve: "retrieved",
-      },
-      always: {
-        target: "archived",
-        guard: "isArchived",
-        // @ts-expect-error the visual editor doesn't support the `guard` property yet
-        cond: "isArchived",
-      },
+      always: [
+        {
+          target: "archived",
+          guard: "isArchived",
+          // @ts-expect-error the visual editor doesn't support the `guard` property yet
+          cond: "isArchived",
+        },
+        {
+          target: "created",
+          guard: "isNew",
+          // @ts-expect-error the visual editor doesn't support the `guard` property yet
+          cond: "isNew",
+        },
+        {
+          target: "retrieved",
+          guard: "isPersisted",
+          // @ts-expect-error the visual editor doesn't support the `guard` property yet
+          cond: "isPersisted",
+        },
+      ],
     },
     created: {
       on: {
